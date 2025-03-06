add21e5b053069ec624a16bd16d08a7550c9f8b9
saramohamed124 <saramohamaden1234@gmail.com>
2025-03-06 18:23:26 +0200

    AuthProfile

--- src/components/Auth/Email/components/resend_email/ContentResend.jsx
+++ src/components/Auth/Email/components/resend_email/ContentResend.jsx
@@ -9,60 +9,79 @@ import TextInput from '../../../common/TextInput'
 import { api } from '../../../../../api/api'
 import { RESEND_EMAIL } from '../../../../../api/endpoints'
 import { ToastWrapper } from '../../../utils/ToasterWrapper'
-import { SuccessMsgToast } from '../../../utils/toasts'
+import { ErrorMsgToast, SuccessMsgToast } from '../../../utils/toasts'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { MAIL_REGEX } from '../../../constants/regex'
 
 const ContentResend = () => {
-    const [email, setEmail] = useState();
 
-    const handleResendEmailVerfication = useCallback(async(e) => {
-        e.preventDefault()
-        try{
-            await api.post(RESEND_EMAIL,
-                {email}
-            );
-            SuccessMsgToast('تم إرسال ايميل التفعيل بنجاح. تحقق من بريدك الالكتروني.')
-        }catch(err){
-
-        }
-    }, [email])
-return (
-    <Box
-        sx={{
-            ...FlexboxCenter,
-            ...FlexBoxCol,
-            width: '100%',
-            gap: '20px',
-            '@media (max-width: 600px)': {
-                justifyContent: 'center',
-                alignItems: 'center',
-            },
-        }}
-    >
-        <Typography sx={{ ...VerfiedEmailText }}>إعادة إرسال ايميل التفعيل</Typography>
-        <Box component={'form'} onSubmit={handleResendEmailVerfication}
-            sx={{
-                ...FlexboxCenter,
-                ...FlexBoxCol,
-                width: '50%',
-                gap: '20px',
-                '@media (max-width: 600px)': {
+        const [email, setEmail] = useState('');
+        const location = useLocation();
+        const navigate = useNavigate();
+        const from = location.state?.from?.pathname || '/verify-email';
+    
+        const handleResendEmailVerfication = useCallback(async (e) => {
+            e.preventDefault();
+        
+            if (!MAIL_REGEX.test(email)) {
+                ErrorMsgToast('يرجى إدخال بريد إلكتروني صحيح.');
+                return;
+            }
+        
+            try {
+                await api.post(RESEND_EMAIL, { email });
+                SuccessMsgToast('تم إرسال ايميل التفعيل بنجاح. تحقق من بريدك الإلكتروني.');
+        
+                setTimeout(() => {
+                    navigate(from, { replace: true });
+                }, 2000);
+            } catch (err) {
+                const errMsg = err.response?.data?.message || 'حدث خطأ أثناء إعادة الإرسال، يرجى المحاولة لاحقاً';
+                ErrorMsgToast(errMsg);
+            }
+        }, [email, from, navigate]);
+        
+        return (
+            <Box
+                sx={{
+                    ...FlexboxCenter,
+                    ...FlexBoxCol,
                     width: '100%',
-                },
-            }}
-        >
-            <TextInput
-                color="black"
-                sx={{ width: '100%' }}
-                id="email"
-                type="email"
-                onChange={(e) => setEmail(e.target.value)}
-                label="أدخل بريدك الإلكتروني لإعادة إرسال رابط التفعيل"
-            />
-            <Button type='submit' sx={{ ...VerfiedEmailBtn }}>إعادة الإرسال</Button>
-        </Box>
-        <ToastWrapper/>
-    </Box>
-);
-}
+                    gap: '20px',
+                    '@media (max-width: 600px)': {
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    },
+                }}
+            >
+                <Typography sx={{ ...VerfiedEmailText }}>إعادة إرسال ايميل التفعيل</Typography>
+                <Box component={'form'} onSubmit={handleResendEmailVerfication}
+                    sx={{
+                        ...FlexboxCenter,
+                        ...FlexBoxCol,
+                        width: '50%',
+                        gap: '20px',
+                        '@media (max-width: 600px)': {
+                            width: '100%',
+                        },
+                    }}
+                >
+                    <TextInput
+                        color="black"
+                        sx={{ width: '100%' }}
+                        id="email"
+                        type="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        onBlur={() => console.log("Blur Event - Email:", email)} // Debugging
+                        label="أدخل بريدك الإلكتروني لإعادة إرسال رابط التفعيل"
+                        errorMsg={'يرجى إدخال ايميل صالح'}
+                    />
+                    <Button type='submit' sx={{ ...VerfiedEmailBtn }}>إعادة الإرسال</Button>
+                </Box>
+                <ToastWrapper/>
+            </Box>
+        );
+    }
+    
 
 export default ContentResend
--- src/components/Navbar/components/AuthProfile.jsx
+++ src/components/Navbar/components/AuthProfile.jsx
@@ -2,16 +2,24 @@ import { Avatar, Box, Button, List, ListItem, styled, Typography } from '@mui/ma
 import React, { useContext } from 'react'
 import { ProverCostum } from '../../../styles/Navbar'
 import {FlexboxBetween} from '../../../styles/globalStyles'
-import avatar from '../assets/imgs/avatar.png';
+import avatar_instructor from '../assets/imgs/avatar_instructor.png';
+import avatar_student from '../assets/imgs/avatar_student.png';
+import courses from '../assets/icons/courses.svg';
+import settings from '../assets/icons/settings.svg';
 import logout_icon from '../assets/icons/logout.svg'
 import { Link } from 'react-router-dom'
 import { getToken, logout } from '../../../services/authServices'
 import  { InstructorProvider } from '../../../context/InstructorInfoProvider'
+import { LinkCostum } from '../Navbar';
+// Cookies
+import Cookies from 'js-cookie';
 
 const AuthProfile = () => {
     const { email } = getToken();
     const { instructor } = useContext(InstructorProvider);
-    
+    // Role
+    const role = Cookies.get('role')
+
     // Style 
     const LinkCustom = styled(Link)({
         color:'blue',
@@ -24,12 +32,17 @@ const AuthProfile = () => {
         ...FlexboxBetween
     })
   return (
+    <>
+    {
+    role === 'Instructor' ?
     <List sx={{...ProverCostum}}>
       <ListItem sx={{FlexboxBetween, gap:'20px',justifyContent:'center'}}>
-        <Avatar src={avatar} alt='avatar'/>
+        <Avatar
+          sx={{ width: 65, height: 65 }}
+          src={avatar_instructor} alt='avatar instructor'/>
         <Box sx={{textAlign:'right'}}>
         <Typography variant='h6'>
-            أهلاً, {instructor.firstName}
+            أهلاً, {instructor?.firstName}
         </Typography>
         <LinkCustom to={'/instructor-profile'}>عرض الملف الشخصي</LinkCustom>
         </Box>
@@ -44,6 +57,45 @@ const AuthProfile = () => {
         </ButtonLogout>
       </ListItem>
     </List>
+    :
+    <List sx={{...ProverCostum}}>
+    
+      <ListItem sx={{FlexboxBetween, gap:'20px',justifyContent:'center'}}>
+        <Avatar
+          sx={{ width: 65, height: 65 }}
+          src={avatar_student} alt='avatar instructor'/>
+        <Box sx={{textAlign:'right'}}>
+        <Typography variant='h6'>
+            أهلاً, {instructor?.firstName}
+        </Typography>
+        <LinkCustom to={'/profile'}>عرض الملف الشخصي</LinkCustom>
+        </Box>
+      </ListItem>
+      <ListItem sx={{FlexboxBetween, gap:'20px',justifyContent:'space-between'}}>
+        <LinkCostum to={'/profile/settings'}>
+          الإعدادات
+        </LinkCostum>
+        <img src={settings} alt='settings'/>
+      </ListItem>
+      <ListItem sx={{FlexboxBetween, gap:'20px',justifyContent:'space-between'}}>
+        <LinkCostum to={'/profile/courses'}>
+          الكورسات
+        </LinkCostum>
+        <img src={courses} alt='courses'/>
+      </ListItem>
+
+      <hr/>
+      <ListItem>
+        <ButtonLogout onClick={() => logout(email)}>
+            <Typography>
+            تسجيل الخروج
+            </Typography>
+            <img src={logout_icon} alt='logout'/>
+        </ButtonLogout>
+      </ListItem>
+    </List>
+    }
+    </>
   )
 }
 
